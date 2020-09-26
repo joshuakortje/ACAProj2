3d902a420c185905d14d31b87c866487bcfe28e1
joshuakortje <38233249+joshuakortje@users.noreply.github.com>
2020-09-26 13:31:52 -0400

    Added Quicksort

--- Project2/src/sorting/QuickSort.java
+++ Project2/src/sorting/QuickSort.java
@@ -5,10 +5,78 @@
  */
 package sorting;
 
+import java.util.Random;
+
 /**
  *
  * @author jkort
  */
 public class QuickSort {
     
+    //Quicksort Algorithm
+    public static void quicksort(int[] A, int p, int r)
+    {
+        if(p < r)
+        {
+            int q = partition(A, p, r);
+            quicksort(A, p, q-1);
+            quicksort(A, q+1, r);
+        }
+    }
+    
+    //partition algorithm
+    private static int partition(int[] A, int p, int r)
+    {
+        int x = A[r]; //pivot element
+        int i = p - 1;
+        
+        int temp;
+        for(int j = p; j < r; j++)
+        {
+            if (A[j] <= x)
+            {
+                i = i+1;
+                swap(A, i, j);
+            }
+        }
+        swap(A, i+1, r);
+        return i+1;
+    }
+    
+    //swap function
+    private static void swap(int[] A, int a, int b)
+    {
+        int temp = A[a];
+        A[a] = A[b];
+        A[b] = temp;
+    }
+    
+    //main for testing purposes
+    public static void main(String[] args)
+    {
+        //make a bunch of random numbers
+        Random rand = new Random(System.nanoTime());
+        int[] testArray = new int[10];
+        for(int i = 0; i < testArray.length; i++)
+        {
+            testArray[i] = rand.nextInt(100);
+        }
+        
+        //Print unsorted array
+        for(int i = 0; i < testArray.length; i++)
+        {
+            System.out.println(testArray[i]);
+        }
+        
+        System.out.println("Sorted numbers:");
+        //Sort
+        quicksort(testArray, 1, testArray.length-1);
+        
+        
+        //Print sorted array
+        for(int i = 0; i < testArray.length; i++)
+        {
+            System.out.println(testArray[i]);
+        }
+    }
 }
